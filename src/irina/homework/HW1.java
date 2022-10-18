c5262e8b3c6ef0353bad55457925eebf5457a295
Yulia Konkova <ubigaia@gmail.com>
2022-10-18 11:51:11 +0300

    My first commit

---
+++ src/irina/homework/HW1.java
@@ -0,0 +1,30 @@
+package irina.homework;
+
+public class HW1 {
+
+    public static void main(String [] args) {
+        
+        int a = 10;
+        int b = 20;
+        int s = 100;
+        int t = s;
+
+        int w, e, r;
+        w = 11;
+        e = 12;
+        r = 15;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(t);
+
+        t = 50;
+        System.out.println(t);
+
+        // t = 50; - попробовать
+        // сначала - объявляем переменную и потом ее иницииализируем
+
+        String myNameInSlack = "Julia Konkova";
+    }
+}
